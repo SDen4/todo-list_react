68258a4b5164000213147886f97a4d1af559ae85
SDen4 <asw325@yandex.ru>
2020-12-09 22:17:19 +0500

    tasks list

---
+++ src/components/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const List = ({ tasks }) => {
+    return (
+        <ul className='list-group'>
+            {tasks.map(task => (
+                <li
+                    className='list-group-item list__item'
+                    key={task.id}
+                >
+                    <div className='list__title_group'>
+                        <strong>{task.title}</strong>
+                        <small>{new Date().toLocaleDateString()}</small>
+                    </div>
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
+                    >&times;</button>
+                </li>
+            ))}
+        </ul>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
 import {Form} from '../components/Form';
+import {List} from '../components/List';
 
 export const Home = () => {
+    const tasks = new Array(3).fill('').map( (_, i) => ({id: i, title: `Task #${i+1}`}));
+
     return (
         <Fragment>
             <Form />
+            <List tasks={tasks}/>
         </Fragment>
     )
 }
